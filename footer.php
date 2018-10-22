9123a587b80ed74529be4a2fa9033cf74035f715
Josh Bell <joshbell@Micheles-MBP.home>
2018-10-22 01:00:50 -0400

    php

--- footer.php
+++ footer.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
-<title>Snack Trax</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+	<head>
+		<title>Snack Trax</title>		
+		<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
 <link rel="stylesheet" href="css/main.css" />
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
 
 
 <!-- Footer -->
@@ -21,3 +23,10 @@
 					&copy; Untitled. Design <a href="https://templated.co">TEMPLATED</a>. Images <a href="https://unsplash.com">Unsplash</a>.
 				</div>
 			</footer>
+
+<!-- Scripts -->
+<script src="js/jquery.min.js"></script>
+			<script src="js/jquery.scrolly.min.js"></script>
+			<script src="js/skel.min.js"></script>
+			<script src="js/util.js"></script>
+			<script src="js/main.js"></script>
--- header.php
+++ header.php
@@ -2,19 +2,12 @@
 <html>
 	<head>
 		<title>Snack Trax</title>		
-		
-<!-- Scripts -->
-<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.scrolly.min.js"></script>
-			<script src="assets/js/skel.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
-
-<meta charset="utf-8" />
+		<meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <link rel="stylesheet" href="css/main.css" />
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
 
 <!-- Header -->
 			<header id="header">
@@ -27,18 +20,10 @@
 				</nav>
 			</header>
 
-		<!-- Menu -->
-			<nav id="menu">
-				<ul class="links">
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="how-it-works.html">How It Works</a></li>
-                    <li><a href="about-us.html">About Us</a></li>
-                    <li><a href="blog.html">Blog</a></li>
-                    <li><a href="sign-up.html">Sign Up!</a></li>
-				</ul>
-				<ul class="actions vertical">
-					<li><a href="#" class="button fit">Login</a></li>
-				</ul>
-			</nav>
-	
-</head>
+<!-- Scripts -->
+			<script src="js/jquery.min.js"></script>
+			<script src="js/jquery.scrolly.min.js"></script>
+			<script src="js/skel.min.js"></script>
+			<script src="js/util.js"></script>
+			<script src="js/main.js"></script>
+
