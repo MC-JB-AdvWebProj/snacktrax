d0a0109b93d6188642cf568afe15bc0519d3845e
Josh Bell <joshbell@Micheles-MBP.home>
2018-10-28 21:00:46 -0400

    passwords

--- blog.php
+++ blog.php
@@ -7,7 +7,7 @@ include 'nav.php';
 ?>
 
 <body>
-<h2 id="elements">Read all about our Snack Trax Members Success Stories Through Our Snack Trax Community Blog!</h2>
+<h2 id="content">Read all about our Snack Trax Members Success Stories Through Our Snack Trax Community Blog!</h2>
   <p>Here at Snack Trax, we are always encouraging the members of our commmunity, to share and express their exprience signing up with us. What you see here on this blog, are personal reviews from members here at Snack Trax. We also ask our members to please provide us the overall rating they would give our company- one out of five stars!</p>
 
 <?php
